06aa5925ee17c3ff8d8cd782ca859b0e0f676cdd
chamith96 <chamithsandagiri@gmail.com>
2018-11-05 15:17:09 +0530

    add components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogShowComponent } from './blog-show/blog-show.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogCreateComponent,
+    BlogShowComponent,
+    BlogComponent,
+    BlogEditComponent,
+    NavComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
